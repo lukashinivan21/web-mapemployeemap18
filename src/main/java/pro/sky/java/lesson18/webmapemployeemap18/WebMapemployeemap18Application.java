f0adf792b8ab7174f57916bac707b9eacbac3354
Ivan <uvanes21@yandex.ru>
2022-03-04 10:30:45 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson18/webmapemployeemap18/WebMapemployeemap18Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson18.webmapemployeemap18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMapemployeemap18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMapemployeemap18Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson18/webmapemployeemap18/WebMapemployeemap18ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson18.webmapemployeemap18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMapemployeemap18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
